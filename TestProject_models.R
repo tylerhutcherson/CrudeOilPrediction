e893fcfd9403b3e4a418bec3416aeefcf1d9658a
Tyler <tch6zf@virginia.edu>
2017-05-03 12:31:21 -0400

    final nnet and added random forest

--- TestProject_models.R
+++ TestProject_models.R
@@ -8,6 +8,7 @@ library(caret)
 library(purrr)
 library(readr)
 library(neuralnet)
+library(nnet)
 library(boot)
 set.seed(123456)
 source("TestProjectScripts.R")
@@ -118,60 +119,89 @@ MSE.lm4 <- sum((pr.lm4 - (((test$Price+1)*(exp2Max-exp2Min)/2)+exp2Min))^2)/nrow
 cv.glm(train,exp2.lm.fit4,K=5)$delta[1]
 
 # Experiment 3 Data - ran into problems no time to worry about it
-set.seed(123456)
-train <- exp3[exp3$Date < "2016-09-01",]
-test <- setdiff(exp3,train)
-exp3.lm.fit1 <- glm(Price~PriceLag1+PriceLag2+PriceLag3+PriceLag4+fCon1Lag1+fCon2Lag1+gold_silverLag1+globalLag1, data=train)
-summary(exp3.lm.fit1)
-pr.lm1 <- ((predict(exp3.lm.fit1,test)+1)*(max(df$Price)-min(df$Price))/2)+min(df$Price)
-MSE.lm1 <- sum((pr.lm1 - (((test$Price+1)*(max(df$Price)-min(df$Price))/2)+min(df$Price)))^2)/nrow(test)
-# RMSE = 0.6529223
-cv.glm(train,exp3.lm.fit1,K=5)$delta[1] 
-
-exp3.lm.fit2 <- glm(Price~PriceLag1+fCon1Lag1+fCon2Lag1+gold_silverLag1+globalLag1, data=train)
-summary(exp3.lm.fit2)
-pr.lm2 <- ((predict(exp3.lm.fit2,test)+1)*(max(df$Price)-min(df$Price))/2)+min(df$Price)
-MSE.lm2 <- sum((pr.lm2 - (((test$Price+1)*(max(df$Price)-min(df$Price))/2)+min(df$Price)))^2)/nrow(test)
-# RMSE = 0.4529021
-cv.glm(train,exp3.lm.fit2,K=5)$delta[1]
-
-exp3.lm.fit3 <- glm(Price~PriceLag1+PriceLag2+PriceLag3+PriceLag4+PriceLag5+PriceLag6+PriceLag7+PriceLag8+PriceLag9+PriceLag10+PriceLag11, data=train)
-summary(exp3.lm.fit3)
-pr.lm3 <- ((predict(exp3.lm.fit3,test)+1)*(max(df$Price)-min(df$Price))/2)+min(df$Price)
-MSE.lm3 <- sum((pr.lm3 - (((test$Price+1)*(max(df$Price)-min(df$Price))/2)+min(df$Price)))^2)/nrow(test)
-# RMSE = 0.269185
-cv.glm(train,exp3.lm.fit3,K=5)$delta[1]  # WINNER
-
-exp3.lm.fit4 <- glm(Price~fCon1Lag1+fCon2Lag1+fCon3Lag1+fCon4Lag1+gold_silverLag1+globalLag1, data=train)
-summary(exp3.lm.fit4)
-pr.lm4 <- ((predict(exp3.lm.fit4,test)+1)*(max(df$Price)-min(df$Price))/2)+min(df$Price)
-MSE.lm4 <- sum((pr.lm4 - (((test$Price+1)*(max(df$Price)-min(df$Price))/2)+min(df$Price)))^2)/nrow(test)
-# RMSE = 0.4813772
-cv.glm(train,exp3.lm.fit4,K=5)$delta[1]
 
 #########################################
 
-set.seed(450)
+# PERFORM MODELING: Neural Network (with Experiment 2 Data)
+
+# multilayer
+set.seed(123456)
 cv.error <- NULL
 k <- 5
-folds <- cut(seq(1,nrow(exp2)),breaks=k*2,labels=FALSE)
-table(folds)
+trainIndex <- createDataPartition(exp2$Price, times=k, p=.8, list=F)
 n <- names(exp2)
 f <- as.formula(paste("Price ~", paste(n[!n %in% c("Price","Date")], collapse = " + ")))
 
 for(i in 1:k){
-  train.cv <- exp2[which(folds==i | folds == i+1 | folds == i+2 | folds == i+3 | folds == i+4,arr.ind=TRUE),]
-  test.cv <- exp2[which(folds==i+5,arr.ind=TRUE),]
-  
-  nn <- neuralnet(f,data=train.cv,hidden=c(9,9),linear.output=T)
-  
+  train.cv <- exp2[trainIndex[,i],]
+  test.cv <- exp2[-trainIndex[,i],]
+  nn <- neuralnet(f,data=train.cv,hidden=c(9,5),linear.output=T)
   pr.nn <- compute(nn,test.cv[,3:27])
   pr.nn <- (pr.nn$net.result+1)*(exp2Max-exp2Min)/2+exp2Min
-  
   test.cv.r <- (test.cv$Price+1)*(exp2Max-exp2Min)/2+exp2Min
-  
   cv.error[i] <- sum((test.cv.r - pr.nn)^2)/nrow(test.cv)
 }
+
+mean(sqrt(cv.error)) # RMSE =
+# not bad - too many parameters to tune
+
+# Singler Layer NNet
+my.grid <- expand.grid(.decay = c(0, 0.05, 0.1), .size = c(5, 7, 9, 12))
+trnCtrl=trainControl(
+  method = "repeatedcv",
+  number=5,
+  repeats=3,
+  search='grid',
+)
+train.cv <- exp2[exp2$Date < "2016-09-01",]
+test.cv <- setdiff(exp2,train.cv)
+caret.fit <- train(f, data = train.cv, trControl = trnCtrl, method = "nnet", maxit = 10000, tuneGrid = my.grid, trace = F, linout = 1) 
+
+pr.nn <- predict(caret.fit$finalModel,test.cv[,3:27])
+pr.nn <- (pr.nn+1)*(exp2Max-exp2Min)/2+exp2Min
+test.cv.r <- (test.cv$Price+1)*(exp2Max-exp2Min)/2+exp2Min
+sqrt(sum((test.cv.r - pr.nn)^2)/nrow(test.cv)) # RMSE = 0.094339
+
+##########################################################
+
+# PERFORM MODELING: Random Forest
+my.grid.rf <- expand.grid(mtry=c(7,9,12,15))
+caret.fit.rf <- train(f, data=train.cv, trControl = trnCtrl, method = 'rf', tuneGrid= my.grid.rf, trace=F, importance=TRUE)
+pr.rf <- predict(caret.fit.rf$finalModel,test.cv[,3:27])
+pr.rf <- (pr.rf+1)*(exp2Max-exp2Min)/2+exp2Min
+test.cv.rf <- (test.cv$Price+1)*(exp2Max-exp2Min)/2+exp2Min
+sqrt(sum((test.cv.rf - pr.rf)^2)/nrow(test.cv)) # RMSE = 0.2444824567
+
+
+# tune tree size (how many trees to grow)
+range <- seq(from = 100, to = 3000, by = 150)
+range %>% map(function(s){
+  randomForest(f, data=train.cv, mtry=15, importance=TRUE, 
+               na.action=na.exclude, ntree = s) %>% 
+    get("mse",.) %>% 
+    mean()
+}) -> tuneTreeSize
+
+plot(range,tuneTreeSize, main="Tree Size and MSE")
+# best # of trees = 2350
+
+
+########################################################
+########################################################
+
+# Best Model = Single Layer Neural Network with 9 hidden units
+
+# plot network
+# build final model on full data except predication date: 4/27/17
+# use tuning parameters from the grid search results
+
+final.model <- nnet(f, data=exp2[1:(nrow(exp2)-1),], maxit = 10000, size = 9, decay = 0)
+prediction <- predict(caret.fit$finalModel,exp2[nrow(exp2),3:27])
+prediction <- (pr.nn+1)*(exp2Max-exp2Min)/2+exp2Min
+actual <- (exp2$Price[nrow(exp2)]+1)*(exp2Max-exp2Min)/2+exp2Min
+difference <- abs(actual-prediction)
+percentDiff <- (difference/actual)*100
+
 
 
 
